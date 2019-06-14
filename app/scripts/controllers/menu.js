ebc9238bae0edfec7330b1e606ad0b23759ac7f3
icsantos <isantos1206@gmail.com>
2019-06-14 02:13:28 -0500

    feat: Use Angular directives to list many items
    - ng-repeat
    - ng-src
    - ng-if
    - ng-class
    - ng-click

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -1,3 +1,4 @@
+/* eslint-disable capitalized-comments */
 /* eslint-disable no-invalid-this */
 /* eslint-env angular*/
 
@@ -12,8 +13,37 @@
  */
 angular.module('udaciMealsApp')
   .controller('MenuCtrl', function () {
-    this.id = 'strawberry-pudding';
-    this.name = 'Strawberry Pudding';
-    this.img = 'strawberry-pudding.jpg';
-    this.rating = 5;
-  });
+    this.items = [
+      {
+          'id': 'chicken-pomegranate-salad',
+          'name': 'Chicken Pomegranate Salad',
+          'img': 'chicken-pomegranate-salad.jpg',
+          'calories': 430,
+          'rating': 4.1
+      },
+      {
+          'id': 'strawberry-pudding',
+          'name': 'Strawberry Pudding',
+          'img': 'strawberry-pudding.jpg',
+          'calories': 280,
+          'rating': 5
+      },
+      {
+          'id': 'ham-goat-cheese-croissant',
+          'name': 'Ham and Goat Cheese Croissant',
+          'img': 'ham-goat-cheese-croissant.jpg',
+          'calories': 280,
+          'rating': 5
+      }
+    ];
+
+    this.increment = function(item) {
+      // item.rating += 0.1;
+      item.rating = ((item.rating * 10) + 1) / 10;
+    };
+
+    this.decrement = function(item) {
+        // item.rating -= 0.1;
+        item.rating = ((item.rating * 10) - 1) / 10;
+    };
+});
